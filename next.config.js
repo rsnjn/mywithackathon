8392a3576f7af8e880733a8247b9660554e8a2e6
rsnjn <riandasanjuan1@gmail.com>
2025-09-09 15:16:50 +1000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
+    unoptimized: true
+  },
+  trailingSlash: false,
+  output: 'standalone'
+}
+
+module.exports = nextConfig
